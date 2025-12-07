d6ebd3b05825ee4433706d3e377fcf607b89ffa3
Jennie Nguyen <nchi09588@gmail.com>
2025-12-07 10:12:23 +0700

    Update start.sh

--- windows_2012_r2/start.sh
+++ windows_2012_r2/start.sh
@@ -169,7 +169,7 @@ start_vm() {
         -spice port=5930,disable-ticketing=on \
         -device virtio-serial \
         -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 \
-        -chardev spicevmc,id=spicechannel0,name=vdagent \
+        -chardev spicevmc,id=spicechannel0,name=vdagent
 }
 
 reset_vm() {
